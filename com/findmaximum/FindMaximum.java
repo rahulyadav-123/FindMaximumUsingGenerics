e479c4cf506e4f26e9162bbe94006dd2c6b50f42
rahul yadav <ry219613@gmail.com>
2021-11-03 00:15:14 +0530

    print max method

--- com/findmaximum/FindMaximum.java
+++ com/findmaximum/FindMaximum.java
@@ -3,21 +3,34 @@ package com.findmaximum;
 public class FindMaximum {
 	//main method
 		public static void main(String args[]) {
-			//string values
+			Integer intVal1 = 12, intVal2 = 13, intVal3 = 9;
+			Float floatVal1 = 12.55f, floatVal2 = 13.65f, floatVal3 = 19.22f;
 			String strVal1 = "Rohit", strVal2 = "Rahul", strVal3 = "Roshani";
 			
-			//assign 1st value to max
-			String max = strVal1;
-			
-			//checking max with other values
-			if (strVal2.compareTo(max) > 0) {
-				max = strVal2;
-			}
-			if (strVal3.compareTo(max) > 0) {
-				max = strVal3;
-			}
-		
-			//printing maximum value among three
-			System.out.println("Amongst the three the maximum is " + max);
+			//call generic method
+			toprint(intVal1, intVal2, intVal3);
+			toprint(floatVal1, floatVal2, floatVal3);
+			toprint(strVal1, strVal2, strVal3);
 		}
-}
+
+		//generic method
+		private static <E extends Comparable<E>> void toprint(E val1, E val2, E val3) {
+			
+			//assign value to max
+			E max = val1;
+			
+			//check value is greater of not
+			if (val2.compareTo(max) > 0) {
+				max = val2;
+			}
+			if (val3.compareTo(max) > 0) {
+				max = val3;
+			}
+			printMax(max);
+		}
+		
+		public static <E> void printMax(E max) {
+			System.out.println("Amongst the three the maximum is " + max);		
+		}
+
+	}
