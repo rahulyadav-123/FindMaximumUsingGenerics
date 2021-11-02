bf7bc74f96f19a426b1130919b42db365e830bdc
rahul yadav <ry219613@gmail.com>
2021-11-03 00:10:27 +0530

    Find maximum among three string

--- com/findmaximum/FindMaximum.java
+++ com/findmaximum/FindMaximum.java
@@ -3,18 +3,18 @@ package com.findmaximum;
 public class FindMaximum {
 	//main method
 		public static void main(String args[]) {
-			//float values
-			Float floatVal1 = 12.21f, floatVal2 = 18.22f, floatVal3 = 9.2f;
+			//string values
+			String strVal1 = "Rohit", strVal2 = "Rahul", strVal3 = "Roshani";
 			
 			//assign 1st value to max
-			Float max = floatVal1;
+			String max = strVal1;
 			
 			//checking max with other values
-			if (floatVal2.compareTo(max) > 0) {
-				max = floatVal2;
+			if (strVal2.compareTo(max) > 0) {
+				max = strVal2;
 			}
-			if (floatVal3.compareTo(max) > 0) {
-				max = floatVal3;
+			if (strVal3.compareTo(max) > 0) {
+				max = strVal3;
 			}
 		
 			//printing maximum value among three
