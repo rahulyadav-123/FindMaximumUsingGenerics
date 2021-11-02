cb034a2c950bfb38f50671661038dd8732f20ce1
rahul yadav <ry219613@gmail.com>
2021-11-02 23:53:40 +0530

    maximum value using generics

--- com/findmaximum/FindMaximum.java
+++ com/findmaximum/FindMaximum.java
@@ -3,38 +3,29 @@ package com.findmaximum;
 public class FindMaximum {
 	//main method
 		public static void main(String args[]) {
-			Integer[] intVal = {12, 13, 9, 5};
-			Float[] floatVal = {12.55f, 13.65f, 19.22f};
-			String[] strVal = {"Ram", "Chahal", "Pahal"};
-
+			Integer intVal1 = 12, intVal2 = 13, intVal3 = 9;
+			Float floatVal1 = 12.55f, floatVal2 = 13.65f, floatVal3 = 19.22f;
+			String strVal1 = "Rohit", strVal2 = "Rahul", strVal3 = "Roshani";
+			
 			//call generic method
-			toprint(intVal);
-			toprint(floatVal);
-			toprint(strVal);
+			toprint(intVal1, intVal2, intVal3);
+			toprint(floatVal1, floatVal2, floatVal3);
+			toprint(strVal1, strVal2, strVal3);
 		}
 
 		//generic method
-		private static <E extends Comparable<E>> void toprint(E[] arrValue) {
-
+		private static <E extends Comparable<E>> void toprint(E val1, E val2, E val3) {
+			
 			//assign value to max
-			E max = arrValue[0];
-
+			E max = val1;
+			
 			//check value is greater of not
-			for (int i = 1; i < arrValue.length; i++) {
-				
-				int result = max.compareTo(arrValue[i]);
-				
-				if(result < 0) {
-					max = arrValue[i];
-				}
+			if (val2.compareTo(max) > 0) {
+				max = val2;
 			}
-			printMax(max);
+			if (val3.compareTo(max) > 0) {
+				max = val3;
+			}
+			System.out.println("Amongst the three the maximum is " + max);
 		}
-
-		public static <E> void printMax(E max) {
-			System.out.println("Amongst the three the maximum is " + max);		
-		}
-
 	}
-
-
